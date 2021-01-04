6e35667bd5c15bc75604dce43b344f5bc8f3a13b
Akihisa Yagi <capra314cabra@gmail.com>
2021-01-04 22:36:14 +0900

    Release v0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bf-gen",
-    version="0.1.0",
+    version="0.1.1",
     author="capra314cabra",
     author_email="capra314cabra@gmail.com",
     description="Supports you to generate Brainf**k code.",
